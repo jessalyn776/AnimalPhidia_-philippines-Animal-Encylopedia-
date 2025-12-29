4f2c1db4ce49f3c9320672d4721a622f57f36f9c
jessalyn776 <jessalyntrogon@gmail.com>
2025-12-30 05:51:11 +0800

    Delete My-backend directory

--- My-backend/My-backend/src/main/java/com/animalphidia/My_backend/repository/AnimalOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.animalphidia.My_backend.repository;
-
-import com.animalphidia.My_backend.model.AnimalOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnimalOrderRepository extends JpaRepository<AnimalOrder, Integer> {
-}
--- My-backend/My-backend/src/main/java/com/animalphidia/My_backend/repository/ConservationStatusRepository.java
+++
@@ -1,9 +0,0 @@
-package com.animalphidia.My_backend.repository;
-
-import com.animalphidia.My_backend.model.ConservationStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConservationStatusRepository extends JpaRepository<ConservationStatus, Integer> {
-}
